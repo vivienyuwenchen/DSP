d9f78eaca55054816a6ce39ad0d9bb156018ec97
Samantha Young <samantha.young@students.olin.edu>
2018-12-05 20:47:34 -0500

    fixig size memory alocation in decoder

--- decoder.v
+++ decoder.v
@@ -4,12 +4,12 @@
 
 module instructiondecoder
 (
-    output [7:0]    OP_dk,     // 15:8 bits op of D type [operation]
-    output [3:0]    OP_s,     // 15:12 bits of I-type & R-type
-    output [3:0]    S,     // 25:21 bits of I-type & R-type
+    output [7:0]    OP_dk,     // 15:8 bits op of D type and K type [operation] 8 bits
+    output [3:0]    OP_s,     // 15:12 bits op for s type
+    output [3:0]    S,     // 11:8 bits in s type instruction
     output [6:0]    D,     // 15:11 bits of R-type
-    output [6:0]   K,  // 15:0 bits of I-type [16-bit immediate]
-    output    ARP,     // 25:0 bit of J-type [target address]
+    output [7:0]    K,  // 7:0 bits of k type [8-bit immediate]
+    output    ARP,     // 1 bit Addres pointing mode
     output [15:0]   ALL,  // 32 bits of instruction from instruction memor
     input  [15:0]   instruction  // 32 bits of instruction from instruction memory
 );
@@ -18,7 +18,7 @@ module instructiondecoder
     assign OP_s = instruction[15:12];
     assign S = instruction[11:8];
     assign D = instruction[6:0];
-    assign K = instruction[6:0];
+    assign K = instruction[7:0];
     assign ARP = instruction[7];
     assign ALL = instruction[15:0];
 
