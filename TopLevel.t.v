ebec84cfe93836848fb0c2725ca91b1dc4b97f1c
lapudvan <lauren.pudvan@gmail.com>
2018-12-07 11:39:00 -0500

    testbench

--- TopLevel.t.v
+++ TopLevel.t.v
@@ -1,4 +1,4 @@
-`include "pipelinedcpu.v"
+`include "toplevel.v"
 
 //------------------------------------------------------------------------
 // Test bench for XOR SUB SLT sequence.
@@ -17,13 +17,13 @@ module cpu_test ();
     always #10 clk = !clk;
 
     // Instantiate CPU
-    cpu cpu(.clk(clk));
+    dap dsp(.clk(clk));
 
     initial begin
 
     $readmemh("../asm/dat/alu_test.dat", cpu.mem.mem,0);
 
-  	$dumpfile("cpu_alu.vcd");
+  	$dumpfile("dspTopLevel.vcd");
   	$dumpvars();
 
   	// Assert reset pulse
@@ -32,22 +32,22 @@ module cpu_test ();
   	reset = 0; #10;
 
     #82450
-    if(cpu.register.RegisterOutput[8] != 32'h1 || cpu.register.RegisterOutput[9] != 32'h3 || cpu.register.RegisterOutput[10] != 32'h2 || cpu.register.RegisterOutput[11] != 32'h3 || cpu.register.RegisterOutput[12] != 32'h2 || cpu.register.RegisterOutput[13] != 32'h5|| cpu.register.RegisterOutput[14] != 32'h6 ) begin// || cpu.register.RegisterOutput[4] != 32'hb || cpu.register.RegisterOutput[8] != 32'hb || cpu.register.RegisterOutput[9] != 32'h37)
+    if(dsp.accumulator.AccumulatorOutput[8] != 32'h1 || dsp.accumulator.AccumulatorOutput[9] != 32'h3 || dsp.accumulator.AccumulatorOutput[10] != 32'h2 || dsp.accumulator.AccumulatorOutput[11] != 32'h3 || dsp.accumulator.AccumulatorOutput[12] != 32'h2 || dsp.accumulator.AccumulatorOutput[13] != 32'h5|| dsp.accumulator.AccumulatorOutput[14] != 32'h6 ) begin// || dsp.accumulator.AccumulatorOutput[4] != 32'hb || cpu.register.RegisterOutput[8] != 32'hb || cpu.register.RegisterOutput[9] != 32'h37)
           $display("----------------------------------------");
           $display("FAILED PIPELINE ALU TEST");
-          $display("$t0$: Expected: %h, ACTUAL: %h", 32'h1, cpu.register.RegisterOutput[8]);
-          $display("$t1$: Expected: %h, ACTUAL: %h", 32'h3, cpu.register.RegisterOutput[9]);
-          $display("$t2$: Expected: %h, ACTUAL: %h", 32'h2, cpu.register.RegisterOutput[10]);
-          $display("$t3$: Expected: %h, ACTUAL: %h", 32'h3, cpu.register.RegisterOutput[11]);
-          $display("$t4$: Expected: %h, ACTUAL: %h", 32'h2, cpu.register.RegisterOutput[12]);
-          $display("$t5$: Expected: %h, ACTUAL: %h", 32'h5, cpu.register.RegisterOutput[13]);
-          $display("$t6$: Expected: %h, ACTUAL: %h", 32'h6, cpu.register.RegisterOutput[14]);
+          $display("$t0$: Expected: %h, ACTUAL: %h", 32'h1, dsp.accumulator.AccumulatorOutput[8]);
+          $display("$t1$: Expected: %h, ACTUAL: %h", 32'h3, dsp.accumulator.AccumulatorOutput[9]);
+          $display("$t2$: Expected: %h, ACTUAL: %h", 32'h2, dsp.accumulator.AccumulatorOutput[10]);
+          $display("$t3$: Expected: %h, ACTUAL: %h", 32'h3, dsp.accumulator.AccumulatorOutput[11]);
+          $display("$t4$: Expected: %h, ACTUAL: %h", 32'h2, dsp.accumulator.AccumulatorOutput[12]);
+          $display("$t5$: Expected: %h, ACTUAL: %h", 32'h5, dsp.accumulator.AccumulatorOutput[13]);
+          $display("$t6$: Expected: %h, ACTUAL: %h", 32'h6, dsp.accumulator.AccumulatorOutput[14]);
           $display("----------------------------------------");
 
           end
    else begin
          $display("----------------------------------------");
-         $display("PASSED PIPELINE ALU TEST");
+         $display("PASSED TEST");
          $display("----------------------------------------");
          end
   	#2000 $finish();
